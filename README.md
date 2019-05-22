2cde4b3fcb426e8189cdc8c0dcaf1ce4f04e695d
Pelayo Sanchez Margareto <pelaym@amazon.com>
2019-05-22 20:48:31 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,51 @@
-# Useful commands
+# Human authentication using Cognito
 
- * `npm run build`   compile typescript to js
- * `npm run watch`   watch for changes and compile
+This module provides a DynamoDB table with three predefined policies for read from, write into and administer it. As usual, it can be deployed standalone or as part of a larger setup
+
+## Module components
+
+This module contains a DynamoDB table with a partition key, and priced on-demand. Three policies allow specific access levels to that table - i.e. `read`, `write`, and `admin`, respectively. These policies can be attached to the users that will ultimately interact with the table, being able to assign more tailored permissions into their statements.
+
+## Default configuration
+
+By default, a table called `MyTable` will be created, containing a String primary key called `Id`.
+
+## Getting started
+
+### Running in one click
+
+I'm working on preparing automated builds of the modules that could be installed in one click. Stay tuned!
+
+### Installing from code
+
+Install the dependency: `npm i @cdk-modules/ddb-storage` or clone this repository. If you just want to install this module standalone, follow this process
+
+```bash
+cd cdk-modules/ddb-storage/
+npm i
+npm run build
+cdk deploy
+```
+
+And a new stack will appear in your account containing this module's resources.
+
+### Integrating it into your app
+
+If you already have a larger CDK app going - or plan to have it - you can use this module to provide your app with the authecation mechanism you'd need. Just install the deependency into your project, and follow this example:
+
+```typescript
+import { DdbStorage } from '@cdk-modules/ddb-storage'
+
+...
+this.storage = new DdbStorage(this, 'DdbStorage', {
+  tableName: 'MyTable',
+  keyName: 'Id'
+});
+...
+```
+
+## Combine modules
+
+### Combine with Cognito Auth
+
+This module, in combination with [cognito-auth](https://github.com/cdk-modules/cognito-auth), will allow you to provide table storage to your users. You could use object-level IAM statement conditions to further limit the access to the table items - i.e. by User Id or OAuth `sub`.
--- lib/index.ts
+++ lib/index.ts
@@ -1,31 +1,65 @@
-import sns = require('@aws-cdk/aws-sns');
-import sqs = require('@aws-cdk/aws-sqs');
-import cdk = require('@aws-cdk/cdk');
+import { Construct } from '@aws-cdk/cdk';
+import { Table, BillingMode, AttributeType } from '@aws-cdk/aws-dynamodb';
+import { Policy, PolicyStatement } from '@aws-cdk/aws-iam'
 
 export interface DdbStorageProps {
-  /**
-   * The visibility timeout to be configured on the SQS Queue, in seconds.
-   *
-   * @default 300
-   */
-  visibilityTimeout?: number;
+
+  tableName: string,
+  keyName: string,
 }
 
-export class DdbStorage extends cdk.Construct {
-  /** @returns the ARN of the SQS queue */
-  public readonly queueArn: string;
+export class DdbStorage extends Construct {
+  
+  /** @returns the DynamoDB table */
+  public readonly table: Table;
 
-  constructor(scope: cdk.Construct, id: string, props: DdbStorageProps = {}) {
+  /** @returns the read access policy */
+  public readonly readAccessPolicy: Policy;
+
+  /** @returns the writes access policy */
+  public readonly writeAccessPolicy: Policy;
+
+  /** @returns the admin access policy */
+  public readonly adminAccessPolicy: Policy;
+
+  constructor(scope: Construct, id: string, props: DdbStorageProps = {
+    keyName: 'Id',
+    tableName: 'MyTable'
+  }) {
     super(scope, id);
-
-    const queue = new sqs.Queue(this, 'DdbStorageQueue', {
-      visibilityTimeoutSec: props.visibilityTimeout || 300
+    
+    this.table = new Table(this, 'Table', {
+      billingMode: BillingMode.PayPerRequest,
+      tableName: props.tableName,
+      partitionKey: {
+        name: props.keyName,
+        type: AttributeType.String
+      }
     });
 
-    const topic = new sns.Topic(this, 'DdbStorageTopic');
+    this.readAccessPolicy = new Policy(this, 'ReadAccessPolicy')
+    this.readAccessPolicy.addStatement(new PolicyStatement()
+      .addActions(
+        'dynamodb:GetItem',
+        'dynamodb:Query'
+      )
+      .addResource(this.table.tableArn)
+    );
 
-    topic.subscribeQueue(queue);
+    this.writeAccessPolicy = new Policy(this, 'WriteAccessPolicy')
+    this.writeAccessPolicy.addStatement(new PolicyStatement()
+      .addActions(
+        'dynamodb:PutItem',
+        'dynamodb:UpdateItem',
+        'dynamodb:DeleteItem'
+      )
+      .addResource(this.table.tableArn)
+    );
 
-    this.queueArn = queue.queueArn;
+    this.adminAccessPolicy = new Policy(this, 'AdminAccessPolicy')
+    this.adminAccessPolicy.addStatement(new PolicyStatement()
+      .addAction('dynamodb:*')
+      .addResource(this.table.tableArn)
+    );
   }
 }
